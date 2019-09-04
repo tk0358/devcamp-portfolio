a6fa2297d592b11893c6a402c59abf697f2f849b
tk0358 <tk0358a@yahoo.co.jp>
2019-09-04 18:21:20 +0900

    Refactored before action for porftolio setting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,12 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
   belongs_to :topic, optional: true
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
