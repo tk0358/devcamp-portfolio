532e36eb99daa4d0975142d6ac8e4c9da294f926
tk0358 <tk0358a@yahoo.co.jp>
2019-10-11 12:33:34 +0900

    Implemented redis library and created comment generator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,5 +20,6 @@
 //= require html.sortable
 //= require cocoon
 //= require gritter
+//= require cable
 //= require turbolinks
 //= require_tree .
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,7 +4,10 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
 
   validates_presence_of :title, :body
+
   belongs_to :topic, optional: true
+
+  has_many :comments, dependent: :destroy
 
   def self.special_blogs
     all
