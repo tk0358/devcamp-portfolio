c9926d68f822072288f3ee331464303221e56a93
tk0358 <tk0358a@yahoo.co.jp>
2019-12-08 14:24:12 +0900

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module DevCampPortfolio
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
