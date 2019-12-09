1aaf2f93f0c51b481bcbb68a2bfede23ec771d6c
tk0358 <tk0358a@yahoo.co.jp>
2019-12-09 22:24:40 +0900

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://tk0358-devcamp-portfolio.herokuapp.com/', 'http://tk0358-devcamp-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://tk0358-devcamp-portfolio.herokuapp.com/cable"
+
 end
